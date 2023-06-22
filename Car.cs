40098e3a3f8eff38ee90660cb9a281e9f7e64a47
thakur17 <42466300+thakur17@users.noreply.github.com>
2023-06-21 21:10:58 -0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apitest
+{
+    public class Car
+    {
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+    }
+}
