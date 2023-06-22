8cfa8b04f5b35c9dc2b9ba8be6174066ccf86c44
thakur17 <42466300+thakur17@users.noreply.github.com>
2023-06-21 21:11:39 -0400

    Add files via upload

---
+++ FruitNewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apitest.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FruitNewController : ControllerBase
+    {
+        List<string> _fruit = new List<string>
+        {
+            "Pear", "Lemon", "Peach"
+        };
+        
+
+        [HttpGet]
+        public ActionResult Update()
+        {
+            return Ok(_fruit);
+        }
+
+        [HttpPost]
+        public ActionResult Update(UpdateModel model)
+        {
+            if (model.Id < 0 || model.Id > _fruit.Count)
+            {
+                return NotFound();
+            }
+            
+            _fruit[model.Id] = model.Name;
+            return Ok(_fruit[model.Id]);
+        }
+    }
+}
---
+++ fruit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apitest.Controllers
+{
+    [ApiController]
+    public class FruuuitController : ControllerBase
+    {
+        private readonly List<string> _fruit = new List<string>
+            {
+                "Pear",
+                "Lemon",
+                "Peach"
+            };
+
+
+        //The [HttpGet("test")] attribute applied to the Index method indicates the method should use
+        //the route template "test" (https://localhost:5001/test) and should respond to HTTP GET requests.
+
+        [HttpGet("test")]
+        public IEnumerable<string> Index()
+        {
+            return _fruit;
+        }
+
+        [HttpGet("fruuuit/{id}")]
+        public ActionResult<string> View(int id)
+        {
+            if (id >= 0 && id < _fruit.Count)
+            {
+                return _fruit[id];
+            }
+
+            return NotFound();
+        }
+    }
+}
